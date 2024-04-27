fb633b5fc6c8b2de18c251aa579a3a516c8be1d1
lLcrowe <leedh7421@naver.com>
2024-04-28 00:54:34 +0900

    타이머모듈에서 액션추가

---
+++ Core/TimerModule_Element_Action.cs
@@ -0,0 +1,20 @@
+﻿namespace lLCroweTool.TimerSystem
+{
+    /// <summary>
+    /// 타 타이머모듈 요소 액션타입
+    /// </summary>
+    public struct TimerModule_Element_Action
+    {
+        public TimerModule_Element timerModule;
+        public System.Action action;
+
+        public void UpdateTimerModule()
+        {
+            if (!timerModule.CheckTimer())
+            {
+                return;
+            }
+            action?.Invoke();
+        }        
+    }
+}
